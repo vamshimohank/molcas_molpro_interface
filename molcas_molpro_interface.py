96f8e50d3ab0119bd0e43824b9360ee13d0498dc
vamshimohank <vamshimohank@gmail.com>
2020-06-23 22:42:48 +0200

    modularized the code

--- molcas_molpro_interface.py
+++ molcas_molpro_interface.py
@@ -1,73 +1,57 @@
-from read_molcas_h5 import *
-from lut import element_Z_from_sym
-from misc import contraction_string
-import re
-
-
 toang = 0.529177
 
-file = '/home/katukuri/work/Molcas/CaCuO2/1site/ANO-R-Zn-sp/B1g/MCPDFT/mcpdft.rasscf.h5'
-unique_atoms = [1, 2, 6, 14, 20]
-point_charges_file = 'cacuo2.evjen.lat'
-ecp_file = 'ecp.dat'
-molpro_inp_file = 'molpro.inp'
+
+def read_basis_data_from_h5(file=''):
+    from read_molcas_h5 import Orbitals
+    from lut import element_Z_from_sym
+
+    import re
+
+    orbs = Orbitals(file, 'hdf5')
+    basis = []
+    z = []
+    names = []
+    coo = []
+    # read all the information from the molcas orbitals class
+    for i in range(len(orbs.centers)):
+        basis.append(orbs.centers[i]['basis'])
+        z.append(orbs.centers[i]['Z'])
+        names.append(orbs.centers[i]['name'])
+        coo.append(orbs.centers[i]['xyz'])
+
+    names_new = []
+    for name in names:
+        names_new.append(re.sub(r'\d+', '', name.split()[0]))
+
+    actual_z = []
+    for name in names_new:
+        actual_z.append(element_Z_from_sym(name))
+    # print(actual_z)
+
+    return coo, names_new, z, actual_z, basis
 
 
-prefix = '''***, Title
-MEMORY, 1000,M;
+# check if ECPS are present
+def check_for_ecps(basis, q):
+    from lut import element_Z_from_sym
+    ecp = []
+    hasecp = False
+    for i in range(len(basis)):
+        if element_Z_from_sym(names[i]) != q[i]:
+            ecp.append(True)
+            hasecp = True
+        else:
+            ecp.append(False)
+    return hasecp, ecp
 
-GPRINT, BASIS
-
-GDIRECT
-
-!Symmetry, NOSYM
-ANGSTROM
-GEOMETRY={
-'''
-out_file = open(molpro_inp_file,'w')
-print(prefix,end='',file=out_file)
-
-orbs = Orbitals(file,'hdf5')
-basis = []
-z = []
-names = []
-coo = []
-# read all the information from the molcas orbitals class
-for i in range(len(orbs.centers)):
-    basis.append(orbs.centers[i]['basis'])
-    z.append(orbs.centers[i]['Z'])
-    names.append(orbs.centers[i]['name'])
-    coo.append(orbs.centers[i]['xyz'])
-
-names_new = []
-for name in names:
-    names_new.append(re.sub(r'\d+', '', name.split()[0]))
-
-actual_z = []
-for name in names_new:
-    actual_z.append(element_Z_from_sym(name))
-print(actual_z)
-
-print(len(coo), file=out_file)
-print('', file=out_file)
-for j, xyz in enumerate(coo):
-    if j+1 in unique_atoms:
-        suffix = unique_atoms.index(j+1) + 1
-    print('%s  %3.7f %3.7f  %3.7f'%(names_new[j]+str(suffix), toang*xyz[0], toang*xyz[1], toang*xyz[2]),file=out_file)
-print('}', file=out_file)
-if point_charges_file :
-    print('{lattice,usesym,infile='+point_charges_file+'}', file=out_file)
-
-#check if ECPS are present
-ecp = []
-for i in range(len(basis)):
-    if element_Z_from_sym(names_new[i]) != z[i]:
-        ecp.append(True)
-        hasecp = True
-    else:
-        ecp.append(False)
 
 def reformat_basis(basis):
+    '''
+    Reformats the basis read from the hdf5 file. This is essential to use the basis_set_exchange molpro writer
+    :param basis: basis read from hdf5 file
+    :return: basis data in the format as in basis_set_exchange
+    '''
+
     data = dict()
     data['electron_shells'] = []
     # print(len(basis))
@@ -89,6 +73,11 @@ def reformat_basis(basis):
 
 
 def create_data_old(basis):
+    '''
+    Obselete function
+    :param basis:
+    :return:
+    '''
     data = dict()
     data['electron_shells'] = []
     for n_am in range(len(basis)):
@@ -108,25 +97,55 @@ def create_data_old(basis):
     return data
 
 
-s = 'basis={\n'
-for i, b in enumerate(basis):
-    if i+1 in unique_atoms :
-        suffix = str(unique_atoms.index(i+1) + 1)
-        if not ecp[i]:
-            data = reformat_basis(b)
-            # s += '! {:20} {}\n'.format(names_new[i] + suffix, contraction_string(data))
-            s += write_molpro(names_new[i] + suffix, data=data)
-            # print(s)
-        else:
-            data = reformat_basis(b)
-            # s += '! {:20} {}\n'.format(names_new[i] + suffix, contraction_string(data))
-            s += write_molpro(names_new[i] + suffix, data=data)
-            # print(s)
-if hasecp:
-    with open(ecp_file) as fp:
-        ecp_dat = fp.read()
+def create_molpro_input(coo, names, basis, molpro_inp_file, hasecp= False, pc_file=''):
+    from read_molcas_h5 import write_molpro
+    out_file = open(molpro_inp_file, 'w')
 
-    s += ecp_dat
-s += '}\n'
-print(s,file=out_file)
-out_file.close()
+    ecp_file = 'ecp.dat'
+    prefix = '''***, Title\n MEMORY, 1000,M; \n \nGPRINT, BASIS \nGDIRECT\n!Symmetry, NOSYM \nANGSTROM \nGEOMETRY={
+    '''
+
+    print(prefix, end='', file=out_file)
+
+    print(len(coo), file=out_file)
+    print('', file=out_file)
+    suffix = ''
+    for j, xyz in enumerate(coo):
+        if j + 1 in unique_atoms:
+            suffix = unique_atoms.index(j + 1) + 1
+        print('%s  %3.7f %3.7f  %3.7f' % (names[j] + str(suffix), toang * xyz[0], toang * xyz[1], toang * xyz[2]),
+              file=out_file)
+    print('}', file=out_file)
+
+    if pc_file != '':
+        print('{lattice,usesym,infile=' + pc_file + '}', file=out_file)
+
+    s = 'basis={\n'
+    for i, b in enumerate(basis):
+        if i + 1 in unique_atoms:
+            suffix = str(unique_atoms.index(i + 1) + 1)
+            if not ecp[i]:
+                data = reformat_basis(b)
+                s += write_molpro(names[i] + suffix, data=data)
+            else:
+                data = reformat_basis(b)
+                s += write_molpro(names[i] + suffix, data=data)
+    if hasecp:
+        with open(ecp_file) as fp:
+            ecp_dat = fp.read()
+        print('ECPs are taken for the provided ecp.dat file')
+        s += ecp_dat
+    s += '}\n'
+    print(s, file=out_file)
+    out_file.close()
+
+
+unique_atoms = [1, 2, 6, 14, 20]
+point_charges_file = 'cacuo2.evjen.lat'
+file = '/home/katukuri/work/Molcas/CaCuO2/1site/ANO-R-Zn-sp/B1g/MCPDFT/mcpdft.rasscf.h5'
+molpro_inp_file = 'molpro.inp'
+
+coo, names, q, z, basis= read_basis_data_from_h5(file=file)
+hasecp, ecp = check_for_ecps(basis,q)
+# print(hasecp, ecp)
+create_molpro_input(coo, names, basis, molpro_inp_file, hasecp=hasecp, pc_file=point_charges_file)
